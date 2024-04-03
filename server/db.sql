988505cb704a289dea45fcffe809f013a025f2d1
lasha-codes <lashagiorgi420@gmail.com>
2024-04-03 09:55:04 -0700

    this doesn;t work so good

--- server/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE blogs (
-  id SERIAL PRIMARY KEY NOT NULL,
-  title VARCHAR(200) NOT NULL,
-  publish_date DATE NOT NULL,
-  description TEXT NOT NULL,
-  image TEXT NOT NULL,
-  email VARCHAR(200) NOT NULL,
-  author VARCHAR(200) NOT NULL
-);
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
-const db = require('./connectDb.js')
 
 const app = express()
 
@@ -9,19 +8,6 @@ const PORT = process.env.PORT || 4000
 
 app.use(cors())
 app.use(express.json())
-
-const getBlogs = async (req, res) => {
-  try {
-    const query = 'SELECT * FROM blogs'
-    const data = await db.query(query)
-    res.status(200).json(data.rows)
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ message: 'Server error.' })
-  }
-}
-
-app.get('/get-blogs', getBlogs)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
